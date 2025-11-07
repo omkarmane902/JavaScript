586620757158add0951ba998b8c88799e2c23df0
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-11-07 22:08:27 +0530

    Add files via upload

---
+++ javaScript/day 2/variable.js
@@ -0,0 +1,27 @@
+let a = 10 ;
+ a = 20;
+
+ a = "omkar mane"
+
+ console.log(a);
+
+
+ var  b = 13;
+
+ var b = 16;
+
+ b = 23
+
+ console.log(b);
+
+
+
+ const  c = 10 ;
+
+ console.log(c);
+ 
+ 
+
+
+
+ 
