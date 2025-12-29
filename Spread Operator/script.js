bafb98f2f9762bcd738527b11a02ba8ba505f281
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-12-30 00:05:00 +0530

    Add files via upload

---
+++ Spread Operator/script.js
@@ -0,0 +1,12 @@
+                                      //!Spread Operator
+
+
+let arr1 = [1,2,3,4];   
+
+let arr2 = [5,6,7,8];
+
+let arr3 = [9 ,10,11,12,13];
+
+let arr4 = [...arr1, ...arr2 , arr3[2]];  
+
+console.log(arr4);
