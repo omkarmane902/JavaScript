3f80431c89cd52b37c242be9566161701efe8121
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-02-21 23:12:40 +0530

    Add files via upload

---
+++ DSA/Reverse_a_string_without_using_built_in_functions.js
@@ -0,0 +1,11 @@
+//! Reverse a string without using built-in functions.
+
+let str = "i love javascript";
+let rev = "";
+
+for (let i = str.length-1; i >= 0; i--) {
+    
+    rev += str[i]
+    
+}
+console.log(rev);
